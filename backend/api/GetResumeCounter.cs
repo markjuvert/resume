fc158640acd897ec653093f2a79c088dac530d56
markjuvert <60376215+markjuvert@users.noreply.github.com>
2022-01-03 17:36:07 -0600

    added connection to database

---
+++ backend/api/GetResumeCounter.cs
@@ -0,0 +1,41 @@
+using System.Net.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+
+
+namespace Company.Function
+{
+    public static class GetResumeCounter
+    {
+        [FunctionName("GetResumeCounter")]
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName:"AzureResume", collectionName:"Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+            [CosmosDB(databaseName:"AzureResume", collectionName:"Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter UpdatedCounter,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            UpdatedCounter = counter;
+
+            UpdatedCounter.Count += 1;
+
+            var JsonToRetun = JsonConvert.SerializeObject(counter);
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonToRetun, Encoding.UTF8, "application/Json")
+            };
+        }
+    }
+}
